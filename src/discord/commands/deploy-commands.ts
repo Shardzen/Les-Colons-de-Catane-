335306a174718a5c8e1db1a6681bc0977e0d0fda
rcombresansq-pixel <rcombresansq@gmail.com>
2026-03-02 11:24:50 +0100

    Add minor modifications on deply command file

--- src/discord/commands/deploy-commands.ts
+++ src/discord/commands/deploy-commands.ts
@@ -1,30 +1,7 @@
 import { REST, Routes, SlashCommandBuilder } from "discord.js";
 import 'dotenv/config'
 
-const commandsData = [join, start, build].map(cmd => cmd.data.toJSON());
 
-const command = new SlashCommandBuilder()
-                .setName('join')
-                .setDescription('Joins an existing game'),
-
-const command = new SlashCommandBuilder()
-                .setName('start')
-                .setDescription('Start new game'),
-
-const command = new SlashCommandBuilder()
-                .setName('build')
-                .setDescription('Build something')
-                .addStringOption(opt=>opt.setName('type'))
-                .setDescription('What do you want to build ?')
-                .setRequired(true)
-                .addChoices(
-                 { name: 'CITY', value: 'CITY' },
-                { name: 'ROAD', value: 'ROAD' },
-                { name: 'SETTLEMENT', value: 'SETTLEMENT' }
-                  )
-                .addCoords(opt=>opt.setName('coords'))
-                .setDescription()
-                .SetRequired(true),
                 
 
 //async function attendre réponse du net
@@ -50,3 +27,29 @@ export async function deployCommands({ guildId }: DeployCommandsProps) {
     console.error(error);
   }
 }
+
+const commandsData = [join, start, build].map(cmd => cmd.data.toJSON());
+
+const JoinCommand = new SlashCommandBuilder()
+                .setName('join')
+                .setDescription('Joins an existing game'),
+
+const StartCommand = new SlashCommandBuilder()
+                .setName('start')
+                .setDescription('Start new game'),
+
+const BuildCommand = new SlashCommandBuilder()
+                .setName('build')
+                .setDescription('Build something')
+                .addStringOption(opt=>opt.setName('type'))
+                .setDescription('What do you want to build ?')
+                .setRequired(true)
+                .addChoices(
+                 { name: 'CITY', value: 'CITY' 
+                 },
+                { name: 'ROAD', value: 'ROAD' },
+                { name: 'SETTLEMENT', value: 'SETTLEMENT' }
+                  )
+                .addStringOption(opt=>opt.setName('coords'))
+                .setDescription()
+                .setRequired(true)
